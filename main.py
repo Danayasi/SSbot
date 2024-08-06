0fd840e11207c17e2531f6a151ba5a60f2c50d67
danayasi <danga0507@mail.ru>
2024-08-06 13:16:42 +0300

    description update

--- main.py
+++ main.py
@@ -13,27 +13,27 @@ async def on_ready():
     print("Готов!")
 
 
-@bot.slash_command(name="jerkoff")
+@bot.slash_command(name="jerkoff", description="Поджеркать.(джерк джерк)")
 async def ping(ctx):
     await ctx.send("https://cdn.discordapp.com/attachments/939879746555232349/1269984047086436403/1666b172eea9558b.gif?ex=66b20c4f&is=66b0bacf&hm=deee799251be8efcd3c804678ef185522c69625e4f541514315facec6c72b1d8&")
 
 
-@bot.slash_command(name="beat")
+@bot.slash_command(name="beat", description="Избей своего друга или врага?")
 async def ping(ctx, who:disnake.Member, tobeat:disnake.Member):
     await ctx.send(f"{who.mention} избивает {tobeat.mention}")
 
-@bot.slash_command(name="random")
+@bot.slash_command(name="random", description="Получить случайное число в промежутке введёных чисел.")
 async def ping(ctx, first_number:int, second_number:int):
     await ctx.send(f"Выбрано число в промежутке от {first_number} до {second_number}. Это число - {random.randint(first_number, second_number)}")
 
 
-@bot.slash_command(name="clear")
+@bot.slash_command(name="clear", description="Очистить X количество сообщений.")
 async def clear(self, interaction, amount: int):
     await interaction.response.send_message(f"Удалено {amount} сообщений.")
     await interaction.channel.purge(limit=amount + 1)
 
 
-@bot.slash_command(name="avatar")
+@bot.slash_command(name="avatar", description="Взять аватар человека.")
 async def avatar(self, interaction, member: disnake.Member = None):
     user = member or interaction.author
     embed = disnake.Embed(title="Аватарка", color=0xff5252)
